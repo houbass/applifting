b12b29d67cd4da1a88eaf9ca0078d02a78fa88ca
Ondrej Laube <laube.ondrej@seznam.cz>
2025-01-26 21:16:41 +0100

    config change

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-}
+  eslint: {
+    ignoreDuringBuilds: true, // Ignores ESLint errors during production builds
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
