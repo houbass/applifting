9d124331e29faf311b511e83c9057b7f7d750d1e
Ondrej Laube <laube.ondrej@seznam.cz>
2025-02-02 18:29:52 +0100

    added dub to styles

--- src/constants/globalConstants.ts
+++ src/constants/globalConstants.ts
@@ -9,7 +9,7 @@ export const INSTRUMENTS = [
 
 export const STYLES = [
   "house", "hip hop", "cinematic", "techno", "trap", "edm", "pop", "tech house", "rnb",
-  "deep house", "downtempo", "soul", "dubstep", "drum and bass", "progressive house",
+  "deep house", "downtempo", "soul", "dubstep", "dub", "drum and bass", "progressive house",
   "ambient", "experimental", "trance", "future bass", "lo-fi hip hop", "synthwave", 
   "funk", "uk garage", "disco", "electro", "future house", "rock", "chillout", "psy trance",
   "bass music", "boom bap", "jazz", "idm", "drumstep", "synth-pop", "drill"
