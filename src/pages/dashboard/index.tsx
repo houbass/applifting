7c06b0124699a0220ca430292aca3a95b95f898f
Ondrej Laube <laube.ondrej@seznam.cz>
2025-02-24 01:05:56 +0100

    create collab button

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from 'next/link';
-import { Box, Button, Stack } from '@mui/material';
+import { Box, Button, Card, Stack } from '@mui/material';
 import useHomeRedirect from '@/hooks/redirects/useHomeRedirect';
 
 // Components
@@ -19,22 +19,25 @@ export default function Dashboard() {
 
       { user && userCheck && (
         <PageLayout>
-          <Stack 
-            gap={2} 
-            flexDirection="row" 
-            justifyContent="center"
-          >
-            <Box pt={2}>
-              <Link href="/createproject" passHref legacyBehavior>
-                <Button 
-                  variant="contained"
-                  color="error"
-                >
-                  Create Collab
-                </Button>
-              </Link>
-            </Box>
-          </Stack>
+
+          <Card sx={{marginTop: "16px"}}>
+            <Stack 
+              gap={2} 
+              flexDirection="row" 
+              justifyContent="center"
+            >
+              <Box p={5}>
+                <Link href="/createproject" passHref legacyBehavior>
+                  <Button 
+                    variant="contained"
+                    color="error"
+                  >
+                    Create Collab
+                  </Button>
+                </Link>
+              </Box>
+            </Stack>
+          </Card>
 
           <Timeline />
         </PageLayout>
