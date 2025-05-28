40fda774a417bb3a87309b5a77808f6a9773b3d1
Ondrej Laube <laube.ondrej@seznam.cz>
2025-05-28 18:14:00 +0200

    delete messages

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
 };
 
 module.exports = nextConfig;
